9449b7cb23f94af189b0cd1246c7ac4b322af377
jonia89 <111957252+jonia89@users.noreply.github.com>
2022-09-09 14:47:27 +0300

    #6345 boolean to string
    Everything works now and instead of boolean, functions return string

---
+++ payment-03.test.js
@@ -0,0 +1,84 @@
+const { paymentProcess } = require("./payment");
+const fetchMock = require("jest-fetch-mock");
+
+fetchMock.enableMocks();
+
+beforeEach(() => {
+  fetch.resetMocks();
+});
+
+describe("flow", () => {
+  it("test the payment flow", async () => {
+    fetch.mockResponseOnce(JSON.stringify({ validCard: true }));
+    fetch.mockResponseOnce(JSON.stringify({ ok: true }));
+
+    const person = {
+      firstName: "James",
+      middleName: "Roger",
+      lastName: "Smith",
+    };
+    const cc = {
+      number: "0123456789012345",
+      cvc: "123",
+    };
+    const payment = { sum: 10 };
+    const paymentIsOk = await paymentProcess(person, cc, payment);
+
+    expect(paymentIsOk).toBe("OK");
+  });
+
+  it("test the payment flow - INVALID_CARD", async () => {
+    fetch.mockResponseOnce(JSON.stringify({ validCard: false }));
+    fetch.mockResponseOnce(JSON.stringify({ ok: true }));
+
+    const person = {
+      firstName: "James",
+      middleName: "Roger",
+      lastName: "Smith",
+    };
+    const cc = {
+      number: "0123456789012345",
+      cvc: "123",
+    };
+    const payment = { sum: 10 };
+    const paymentIsOk = await paymentProcess(person, cc, payment);
+
+    expect(paymentIsOk).toBe("INVALID_CARD");
+  });
+
+  it("test the payment flow - INVALID_PERSON", async () => {
+    fetch.mockResponseOnce(JSON.stringify({ validCard: true }));
+    fetch.mockResponseOnce(JSON.stringify({ ok: true }));
+
+    const person = {
+      firstName: "James",
+      middleName: "Roger",
+    };
+    const cc = {
+      number: "0123456789012345",
+      cvc: "123",
+    };
+    const payment = { sum: 10 };
+    const paymentIsOk = await paymentProcess(person, cc, payment);
+
+    expect(paymentIsOk).toBe("INVALID_PERSON");
+  });
+  it("test the payment flow - OK", async () => {
+    fetch.mockResponseOnce(JSON.stringify({ validCard: true }));
+    fetch.mockResponseOnce(JSON.stringify({ ok: true }));
+
+    const person = {
+      firstName: "James",
+      middleName: "Roger",
+      lastName: "Smith",
+    };
+    const cc = {
+      number: "0123456789012345",
+      cvc: "123",
+    };
+    const payment = { sum: 10 };
+    const paymentIsOk = await paymentProcess(person, cc, payment);
+
+    expect(paymentIsOk).toBe("OK");
+  });
+});
